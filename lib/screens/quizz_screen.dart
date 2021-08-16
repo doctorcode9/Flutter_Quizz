9e26a653eeed3793c00ab1a8323e4efc77468eff
doctorcode9 <mouheb.boucherb@gmail.com>
2021-08-16 23:53:31 +0100

    creating the quizz game screen

---
+++ lib/screens/quizz_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuizzScreen extends StatefulWidget {
+  const QuizzScreen({Key? key}) : super(key: key);
+
+  @override
+  _QuizzScreenState createState() => _QuizzScreenState();
+}
+
+class _QuizzScreenState extends State<QuizzScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
