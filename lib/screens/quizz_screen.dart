58f91c90fc3eafe9457d111e233db26700885aa6
doctorcode9 <mouheb.boucherb@gmail.com>
2021-08-17 00:16:20 +0100

    created the basic of the quizz game layout

--- lib/screens/quizz_screen.dart
+++ lib/screens/quizz_screen.dart
@@ -13,6 +13,64 @@ class _QuizzScreenState extends State<QuizzScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.pripmaryColor,
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Text(
+                "Question 1/10",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+            Divider(
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 200.0,
+              child: Text(
+                "Your Question Goes Here",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                ),
+              ),
+            ),
+            RawMaterialButton(
+              constraints: BoxConstraints(minWidth: double.infinity),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  side: BorderSide(
+                    color: AppColor.secondaryColor,
+                    width: 4.0,
+                  )),
+              onPressed: () {},
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 18.0),
+                child: Text(
+                  "Answer 1",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
