72ddbcabed3bc1a737a21faa494b2066d0d0b1ba
doctorcode9 <mouheb.boucherb@gmail.com>
2021-08-16 23:48:34 +0100

    finished the Main menu creen of the app

--- lib/screens/main_menu.dart
+++ lib/screens/main_menu.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:quizz_app/ui/shared/color.dart';
 
@@ -13,6 +14,58 @@ class _MainMenuState extends State<MainMenu> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.pripmaryColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 48.0,
+          horizontal: 12.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                "Quizz App",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: RawMaterialButton(
+                  onPressed: () {},
+                  shape: StadiumBorder(),
+                  fillColor: AppColor.secondaryColor,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 12.0, horizontal: 24.0),
+                    child: Text(
+                      "Start the Quizz",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Made with ❤ by Mouheb Boucherb",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
